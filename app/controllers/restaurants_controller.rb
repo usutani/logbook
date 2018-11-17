1eda1d5fa75fc630c209d79ec590412704634a96
Yasuhiro Usutani <usutani@tgl.co.jp>
2018-11-17 14:29:56 +0900

    Add create action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,4 +11,16 @@ class RestaurantsController < ApplicationController
 
   def edit
   end
+
+  def create
+    restaurant = Restaurant.new(restaurant_params)
+    restaurant.save!
+    redirect_to restaurants_url, notice: "#{Restaurant.model_name.human}「#{restaurant.name}」を登録しました。"
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :url, :note)
+  end
 end
