e604e4ca2a796062a3f64d426e31c4c3ee13c7bf
Yasuhiro Usutani <usutani@tgl.co.jp>
2018-11-17 16:12:48 +0900

    bin/rails g migration ChangeRestaurantsNameNotNull

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_031107) do
+ActiveRecord::Schema.define(version: 2018_11_17_070946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "restaurants", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "url"
     t.text "note"
     t.datetime "created_at", null: false
