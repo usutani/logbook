9ad8599b98a8b3423edeb0ea506d7943f85080c6
Yasuhiro Usutani <usutani@tgl.co.jp>
2018-11-17 15:02:13 +0900

    Add edit action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,12 +12,19 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def create
     restaurant = Restaurant.new(restaurant_params)
     restaurant.save!
     redirect_to restaurants_url, notice: "#{Restaurant.model_name.human}「#{restaurant.name}」を登録しました。"
+  end
+
+  def update
+    restaurant = Restaurant.find(params[:id])
+    restaurant.update!(restaurant_params)
+    redirect_to restaurants_url, notice: "#{Restaurant.model_name.human}「#{restaurant.name}」を更新しました。"
   end
 
   private
