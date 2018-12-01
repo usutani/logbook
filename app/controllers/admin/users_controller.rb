16d765e711c2b2b80b71e514018ec8287a6ac170
Yasuhiro Usutani <usutani@tgl.co.jp>
2018-12-01 16:03:32 +0900

    Add user update and delete

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,11 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -24,9 +25,26 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザ「#{@user.name}」を削除しました。"
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
