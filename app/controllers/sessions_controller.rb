eb2f919be32f08954598db6a667c84c2ab6bfcf4
Yasuhiro Usutani <usutani@tgl.co.jp>
2018-12-01 16:41:01 +0900

    Add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました。'
+  end
+
   private
 
   def session_params
