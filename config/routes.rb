c8e0a6e040801c0244be1cccc8539713a3172261
Yasuhiro Usutani <usutani@tgl.co.jp>
2018-12-01 14:43:31 +0900

    bin/rails g controller Admin::Users index show new edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/new'
+    get 'users/edit'
+  end
   root to: "restaurants#index"
   resources :restaurants
 end
