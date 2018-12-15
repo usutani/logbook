9a4921a93bb2700f7f658c3f53392bb406d94896
Yasuhiro Usutani <usutani@tgl.co.jp>
2018-12-15 13:26:55 +0900

    Commonize with let

--- spec/system/restaurants_spec.rb
+++ spec/system/restaurants_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
 describe '飲食店管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+
   describe '一覧表示機能' do
     before do
-      # ユーザーAを作成しておく
       # 作成者がユーザAである飲食店を作成しておく
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
       FactoryBot.create(:restaurant, name: '最初の飲食店', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
     context 'ユーザーAがログインしているとき' do
-      before do
-        # ユーザAでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_a }
 
       it 'ユーザーAが作成した飲食店が表示される' do
         # 作成済みの飲食店がの名称が画面上に表示されていることを確認
@@ -25,15 +24,7 @@ describe '飲食店管理機能', type: :system do
     end
 
     context 'ユーザーBがログインしているとき' do
-      before do
-        # ユーザーBを作成しておく
-        user_b = FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
-        # ユーザBでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_b }
 
       it 'ユーザーAが作成した飲食店が表示されない' do
         # ユーザーAが作成した飲食店の名称が画面上に表示されていないことを確認
