5dc3b4c26157c57cb538f525ee325a9c9dffe05d
Yasuhiro Usutani <usutani@tgl.co.jp>
2018-12-15 13:28:40 +0900

    Remove comment

--- spec/system/restaurants_spec.rb
+++ spec/system/restaurants_spec.rb
@@ -6,7 +6,6 @@ describe '飲食店管理機能', type: :system do
 
   describe '一覧表示機能' do
     before do
-      # 作成者がユーザAである飲食店を作成しておく
       FactoryBot.create(:restaurant, name: '最初の飲食店', user: user_a)
       visit login_path
       fill_in 'メールアドレス', with: login_user.email
@@ -18,7 +17,6 @@ describe '飲食店管理機能', type: :system do
       let(:login_user) { user_a }
 
       it 'ユーザーAが作成した飲食店が表示される' do
-        # 作成済みの飲食店がの名称が画面上に表示されていることを確認
         expect(page).to have_content '最初の飲食店'
       end
     end
@@ -27,7 +25,6 @@ describe '飲食店管理機能', type: :system do
       let(:login_user) { user_b }
 
       it 'ユーザーAが作成した飲食店が表示されない' do
-        # ユーザーAが作成した飲食店の名称が画面上に表示されていないことを確認
         expect(page).to have_no_content '最初の飲食店'
       end
     end
